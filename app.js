1610c690f9b1ab65b57a7ad093af352eae68accf
Mervinnnnn <mervinhuang08@gmail.com>
2026-07-02 11:57:03 +0700

    Final Project

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.engine('handlebars', engine({
 }))
 
 app.set('view engine', 'handlebars')
-app.set('views', './views')
+app.set('views', './Views')
 app.use(express.static('public'))
 
 app.use(express.urlencoded({
